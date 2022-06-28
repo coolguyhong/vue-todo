431a25b54db9bf3dacb2ad0bcbc7a68174b611b5
coolguyhong <podolsmith@naver.com>
2022-06-28 15:16:03 +0900

    todo application

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  // js script
+}
+</script>
+
+<!-- scoped: 해당 template에서만 적용 -->
+<style scoped>
+h1 {
+  color: #2F3B52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
+</style>
