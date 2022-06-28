8a58df474578ad1e499bf907200a40850b1a02a1
coolguyhong <podolsmith@naver.com>
2022-06-28 20:56:18 +0900

    apply mutation

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -15,13 +15,19 @@
 
 <script>
 export default {
-  props: ['propsdata'],
+  name: 'TodoList',
   methods: {
     removeTodo(todoItem, index) {
-      this.$emit('removeItem', todoItem, index)
+      this.$store.commit('removeOneItem', {
+        todoItem,
+        index
+      })
     },
     toggleComplete(todoItem, index) {
-      this.$emit('toggleItem', todoItem, index)
+      this.$store.commit('toggleOneItem', {
+        todoItem,
+        index
+      })
     }
   },
 }
--- src/store/store.js
+++ src/store/store.js
@@ -20,6 +20,26 @@ const storage = {
 export const store = createStore({
   state: {
     todoItems: storage.fetch()
+  },
+  mutations: {
+    addOneItem(state, todoItem) {
+      const obj = {completed: false, item: todoItem}
+      localStorage.setItem(todoItem, JSON.stringify(obj)) // Javascript 객체를 string으로 변환
+      state.todoItems.push(obj)
+    },
+    removeOneItem(state, payload) {
+      localStorage.removeItem(payload.todoItem.item)
+      state.todoItems.splice(payload.index, 1)
+    },
+    toggleOneItem(state, payload) {
+      state.todoItems[payload.index].completed = !this.todoItems[payload.index].completed
+      localStorage.removeItem(payload.todoItem.item)
+      localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem))
+    },
+    clearAll(state) {
+      localStorage.clear()
+      state.todoItems = []
+    }
   }
 })
 
