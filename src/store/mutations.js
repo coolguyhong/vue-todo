d508f0a9c9894f5e7d6362bc5fc4a83f3784adc8
coolguyhong <podolsmith@naver.com>
2022-06-29 13:07:08 +0900

    store 모듈화

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,23 +1,23 @@
-const addOneItem = (state, todoItem) => {
-  const obj = {completed: false, item: todoItem}
-  localStorage.setItem(todoItem, JSON.stringify(obj)) // Javascript 객체를 string으로 변환
-  state.todoItems.push(obj)
-}
+// const addOneItem = (state, todoItem) => {
+//   const obj = {completed: false, item: todoItem}
+//   localStorage.setItem(todoItem, JSON.stringify(obj)) // Javascript 객체를 string으로 변환
+//   state.todoItems.push(obj)
+// }
 
-const  removeOneItem = (state, payload) => {
-  localStorage.removeItem(payload.todoItem.item)
-  state.todoItems.splice(payload.index, 1)
-}
+// const  removeOneItem = (state, payload) => {
+//   localStorage.removeItem(payload.todoItem.item)
+//   state.todoItems.splice(payload.index, 1)
+// }
 
-const toggleOneItem = (state, payload) => {
-  state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed
-  localStorage.removeItem(payload.todoItem.item)
-  localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem))
-}
+// const toggleOneItem = (state, payload) => {
+//   state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed
+//   localStorage.removeItem(payload.todoItem.item)
+//   localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem))
+// }
 
-const clearAll= (state) => {
-  localStorage.clear()
-  state.todoItems = []
-}
+// const clearAll= (state) => {
+//   localStorage.clear()
+//   state.todoItems = []
+// }
 
-export { addOneItem, removeOneItem, toggleOneItem, clearAll }
+// export { addOneItem, removeOneItem, toggleOneItem, clearAll }
--- src/store/store.js
+++ src/store/store.js
@@ -1,31 +1,15 @@
 import { createApp } from 'vue'
 import { createStore } from 'vuex'
-import * as getters from './getters'
-import * as mutations from './mutations'
+import todoApp from './modules/todoApp'
 // import todo from 'modules/todo.js'
 import App from '../App.vue'
 
-const storage = {
-  fetch() {
-    const arr = [];
-    if (localStorage.length > 0) {
-      for (let i = 0; i < localStorage.length; i++) {
-        if (localStorage.key(i) === '') {
-           continue
-        }
-        arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))))
-      }
-    }
-    return arr;
-  }
-}
+
 
 export const store = createStore({
-  state: {
-    todoItems: storage.fetch()
-  },
-  getters: getters,
-  mutations: mutations
+  modules: {
+    todoApp
+  }
 })
 
 // export const store = createStore({
